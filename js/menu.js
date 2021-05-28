eb3264e94ad6321f205a9db99a1d42cdac72072e
folathecoder <officialakinloyefolarin@gmail.com>
2021-05-28 14:14:33 +0100

    Menu ==== Faded Navigation Menu On Hover

--- js/menu.js
+++ js/menu.js
@@ -2,9 +2,11 @@
 
 //TODO: Target all the required elements
 
+const body = document.body;
+const headerContainer = document.querySelector('.header__container');
 const connectSubMenu = document.querySelector('#connect');
 const connectMenu = document.querySelector('.connect');
-const body = document.body;
+
 
 const hiddenToggle = function() {
     connectSubMenu.classList.toggle('hidden');
@@ -28,4 +30,32 @@ connectSubMenu.addEventListener('mouseout', () => {
 //* Add (hover) event listener to "body" and insert hidden class
 body.addEventListener('click', () => {
     connectSubMenu.classList.add('hidden');
+})
+
+
+//TODO: Fade sibling menu links and logo when hovered
+
+//* Fade Function
+const menuFade = function (e, opacity) {
+    //* Target all elements to be faded on hover
+    if (e.target.classList.contains('menu__link')) {
+        const link = e.target;
+        const siblings = link.closest('.header__container').querySelectorAll('.menu__link');
+        const logo = link.closest('.header__container').querySelector('img');
+
+        //* Add opacity 
+        logo.style.opacity = opacity;
+        siblings.forEach(sibling => {
+            if (sibling !== link) {
+                sibling.style.opacity = opacity;
+            }; 
+        });
+    }
+}
+
+headerContainer.addEventListener('mouseover', function (e) {
+    menuFade(e, 0.5);
+})
+headerContainer.addEventListener('mouseout', function (e) {
+    menuFade(e, 1);
 })
