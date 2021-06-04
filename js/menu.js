030b7446561eaa90f3cb6a0259bfc9ba68846e2f
folathecoder <officialakinloyefolarin@gmail.com>
2021-06-04 10:40:54 +0100

    Mobile Dropdown ==== Completed

--- js/menu.js
+++ js/menu.js
@@ -4,9 +4,38 @@
 
 const body = document.body;
 const headerContainer = document.querySelector('.header__container');
-const hamburgerOpen = document.querySelector('.mobile__menu-icon');
+const hamburgerOpen = document.querySelector('.mobile__menu-icon img');
 const hamburgerClose = document.querySelector('.mobile__menu-icon--close');
 const desktopMenu = document.querySelector('.desktop__menu');
+const navMenu = document.querySelectorAll('.nav__menu');
+const subMenu = document.querySelectorAll('.sub-menu');
+
+// function myFunction(x) {
+//     if (x.matches) { // If media query matches
+//       document.body.style.backgroundColor = "yellow";
+//     } else {
+//      document.body.style.backgroundColor = "pink";
+//     }
+//   }
+  
+//   var x = window.matchMedia("(max-width: 700px)")
+//   myFunction(x) // Call listener function at run time
+//   x.addListener(myFunction) // Attach listener function on state changes
+
+
+// let mqTablet = window.matchMedia("(max-width: 992px)");
+
+// if (mqTablet.matches) {
+//     body.style.backgroundColor = '#3221ce';
+//     console.log(`Folarin`)
+// }
+// else {
+//     body.style.backgroundColor = '#ffffff';
+// }
+
+//TODO: Mobile dropdown menu
+
+//* Main Menu Dropdown 
 
 const hamburgerOpened = function (e) {
     hamburgerClose.classList.remove('hidden');
@@ -30,6 +59,25 @@ hamburgerClose.addEventListener('click', e => {
     hamburgerClosed();
 })
 
+
+//* Sub-Menu Dropdown
+
+subMenu.forEach( menu => {
+    menu.classList.add('hidden');
+})
+
+desktopMenu.addEventListener('click', e => {
+
+    if (e.target.closest('.nav__menu')) {
+        const clicked = e.target.closest('.nav__menu');
+
+        const subDrop = document.querySelector(`.sub-menu--${clicked.dataset.dropdown}`);
+
+        subDrop.classList.toggle('hidden');
+
+        // console.log(subDrop);
+    }
+})
 
 
 //TODO: Fade sibling menu links and logo when hovered
